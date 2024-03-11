960f972a14769a1dcac92b1d9bf6651f0b14a0cb
Jackljk <94715891+jackljk@users.noreply.github.com>
2024-03-10 17:26:28 -0700

    added some boilerplate stuff

---
+++ main.py
@@ -0,0 +1,29 @@
+import torch
+from torch import nn
+from torchvision import transforms as T
+from PIL import Image
+import numpy as np
+from pathlib import Path
+from collections import deque
+import random, datetime, os
+
+# Gym is an OpenAI toolkit for RL
+import gym
+from gym.spaces import Box
+from gym.wrappers import FrameStack
+
+
+# Super Mario environment for OpenAI Gym
+# Load tetris environment
+
+
+
+def train():
+    ...
+
+
+def evaluate():
+    ...
+
+if __name__ == "__main__":
+    ...
