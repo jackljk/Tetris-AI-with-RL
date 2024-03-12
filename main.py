ec9d3f21334d1233d491d71abe37c65fda21bfa2
Jackljk <94715891+jackljk@users.noreply.github.com>
2024-03-11 19:34:22 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,53 @@
 import torch
-from torch import nn
-from torchvision import transforms as T
-from PIL import Image
-import numpy as np
-from pathlib import Path
-from collections import deque
 import random, datetime, os
 from agent import *
-
-# Gym is an OpenAI toolkit for RL
-import gym
 from nes_py.wrappers import JoypadSpace
 import gym_tetris
 from gym_tetris.actions import MOVEMENT
+from model import DQN
+from torch import optim
+import math
 
 
+BATCH_SIZE = 128
+GAMMA = 0.99
+EPS_START = 0.9
+EPS_END = 0.05
+EPS_DECAY = 1000
+TAU = 0.005
+LR = 1e-4
+env = gym_tetris.make('TetrisA-v0')
+env = JoypadSpace(env, MOVEMENT)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Get number of actions from gym action space
+n_actions = env.action_space.n
+# Get the number of state observations
+state, info = env.reset()
+n_observations = len(state)
+
+policy_net = DQN(n_observations, n_actions).to(device)
+target_net = DQN(n_observations, n_actions).to(device)
+target_net.load_state_dict(policy_net.state_dict())
+
+optimizer = optim.AdamW(policy_net.parameters(), lr=LR, amsgrad=True)
+
+
+
+def select_action(state):
+    global steps_done
+    sample = random.random()
+    eps_threshold = EPS_END + (EPS_START - EPS_END) * \
+        math.exp(-1. * steps_done / EPS_DECAY)
+    steps_done += 1
+    if sample > eps_threshold:
+        with torch.no_grad():
+            # t.max(1) will return the largest column value of each row.
+            # second column on max result is index of where max element was
+            # found, so we pick action with the larger expected reward.
+            return policy_net(state).max(1).indices.view(1, 1)
+    else:
+        return torch.tensor([[env.action_space.sample()]], device=device, dtype=torch.long)
 
 
 def train():
@@ -31,6 +64,18 @@ def train():
         done = False
 
         while not done:
+            # Select and perform an action
+            action = select_action(state)
+            next_state, reward, done, info = env.step(action.item())
+            next_state = torch.tensor(next_state, dtype=torch.float32, device=device).unsqueeze(0)
+            reward = torch.tensor([reward], device=device)
+
+            # Move to the next state
+            state = next_state
+
+            # Perform one step of the optimization (on the target network)
+            optimize_model()
+
 
 
 
@@ -40,14 +85,4 @@ def evaluate():
     ...
 
 if __name__ == "__main__":
-    env = gym_tetris.make('TetrisA-v0')
-    env = JoypadSpace(env, MOVEMENT)
-
-    done = True
-    for step in range(5000):
-        if done:
-            state = env.reset()
-        state, reward, done, info = env.step(env.action_space.sample())
-        env.render()
-
-    env.close()
+    
